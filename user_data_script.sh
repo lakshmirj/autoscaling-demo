35aa82eeff6c38b6cec4d0099820aa48d97c6677
lakshmirj <82259644+lakshmirj@users.noreply.github.com>
2022-12-14 12:30:34 +0530

    Update user_data_script.sh

--- user_data_script.sh
+++ user_data_script.sh
@@ -17,7 +17,7 @@ sudo yum upgrade
 sudo yum install git -y
 cd /home/ec2-user
 # get source code from githubt
-git clone https://github.com/felixyu9/auto-scaling-nodejs-app
+git clone https://github.com/lakshmirj/autoscaling-demo.git
 #get in project dir
 cd autoscaling-demo
 #give permission
